92dd1e77bc97f80e44e80c4ce54088db2e58e5e7
SMFCgeeky7 <sebastian_fernandez_conde@hotmail.com>
2022-08-25 16:14:42 -0400

    password generator 0.1

--- JS/script.js
+++ JS/script.js
@@ -1,43 +1,4 @@
     function save_btn() {
     let inpWebsite = document.getElementById("inpWebsite").value;
     document.getElementById("lblWebsiteOutput").innerText = inpWebsite;
-
-    let inpWebsite1 = document.getElementById("inpWebsite2").value;
-    document.getElementById("lblWebsiteOutput1").innerText = inpWebsite1;
-
-    let inpWebsite2 = document.getElementById("inpWebsite").value;
-    document.getElementById("lblWebsiteOutput2").innerText = inpWebsite2;
-
-    let inpWebsite3 = document.getElementById("inpWebsite").value;
-    document.getElementById("lblWebsiteOutput3").innerText = inpWebsite3;
-
-    let inpWebsite4 = document.getElementById("inpWebsite").value;
-    document.getElementById("lblWebsiteOutput4").innerText = inpWebsite4;
-
-    let inpWebsite5 = document.getElementById("inpWebsite").value;
-    document.getElementById("lblWebsiteOutput5").innerText = inpWebsite5;
-
-    let inpWebsite6 = document.getElementById("inpWebsite").value;
-    document.getElementById("lblWebsiteOutput6").innerText = inpWebsite6;
-
-    let inpWebsite7 = document.getElementById("inpWebsite").value;
-    document.getElementById("lblWebsiteOutput7").innerText = inpWebsite7;
-
-    let inpWebsite8 = document.getElementById("inpWebsite").value;
-    document.getElementById("lblWebsiteOutput8").innerText = inpWebsite8;
-
-    let inpWebsite9 = document.getElementById("inpWebsite").value;
-    document.getElementById("lblWebsiteOutput9").innerText = inpWebsite9;
-
-    let inpWebsite10 = document.getElementById("inpWebsite").value;
-    document.getElementById("lblWebsiteOutput10").innerText = inpWebsite10;
-
-    let inpWebsite11 = document.getElementById("inpWebsite").value;
-    document.getElementById("lblWebsiteOutput11").innerText = inpWebsite11;
-
-    let inpWebsite12 = document.getElementById("inpWebsite").value;
-    document.getElementById("lblWebsiteOutput12").innerText = inpWebsite12;
-
-    let inpWebsite13 = document.getElementById("inpWebsite").value;
-    document.getElementById("lblWebsiteOutput13").innerText = inpWebsite13;
     }
